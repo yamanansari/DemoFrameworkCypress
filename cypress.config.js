426dcc09c057e5dadd9e75b8325a66e38f1e40f6
Mohd Yaman Ansari <yamanansari786@gmail.com>
2024-07-11 17:15:20 +0530

    update browser
    update

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'wiihzt',
-  browser: 'chrome',
   viewportWidth: 1280,
   viewportHeight: 720,
   defaultCommandTimeout:10000,
