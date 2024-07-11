78b0a4416acef8ed8c49173f386dc6af0c07e79b
Mohd Yaman Ansari <yamanansari786@gmail.com>
2024-07-11 16:52:34 +0530

    Update constants.js
    update

--- cypress/support/constants.js
+++ cypress/support/constants.js
@@ -1,6 +1,6 @@
  export const url = 'https://www.amazon.in/';
- export const validEmail = 'yaman.neutron@gmail.com';
- export const validPassword = 'yaman@786';
+ export const validEmail = '7417128698';
+ export const validPassword = 'Yaman@786#';
  export const reviewTitle = 'Great Product!';
  export const reviewText = 'I really enjoyed using this product. It exceeded my expectations in every way.';
  export const emailError = 'Invalid email address.'
